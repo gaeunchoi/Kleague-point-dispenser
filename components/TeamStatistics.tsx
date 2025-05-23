9ce990b0415b89b98dd63666cd5f89c13085deb3
gaeunchoi <gaeun1884@naver.com>
2025-05-23 20:47:00 +0900

    Feat: SubStats Tab 기능 추가

---
+++ components/TeamStatistics.tsx
@@ -0,0 +1,5 @@
+function TeamStatistics() {
+  return <div>TeamStatistics임다</div>;
+}
+
+export default TeamStatistics;
--- components/TeamSubStats.tsx
+++ components/TeamSubStats.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import LeagueRanks from "./LeagueRanks";
+import TeamStatistics from "./TeamStatistics";
+import PythagoreanAnalysis from "./PythagoreanAnalysis";
+
 function TeamSubStats() {
-  return <div>TeamSubStats입니당</div>;
+  const tabs: string[] = ["리그 순위", "팀 통계", "피타고리안 분석"];
+  const contents = [
+    <LeagueRanks key={0} />,
+    <TeamStatistics key={1} />,
+    <PythagoreanAnalysis key={2} />,
+  ];
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="w-full mt-4">
+      <div className="grid grid-cols-3 gap-0">
+        {tabs.map((tab, idx) => (
+          <button
+            key={idx}
+            onClick={() => setActiveTab(idx)}
+            className={`py-2 text-center rounded-t-2xl ${
+              activeTab === idx
+                ? "border-b-3 border-blue-500 font-semibold bg-white"
+                : "border-b-3 border-gray-100"
+            }`}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+      <div>{contents[activeTab]}</div>
+    </div>
+  );
 }
 
 export default TeamSubStats;
