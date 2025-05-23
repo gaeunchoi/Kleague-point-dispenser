6186e8cfc7908e43d3fc7147eb44faedbc977605
gaeunchoi <gaeun1884@naver.com>
2025-05-23 17:31:53 +0900

    Feat: CardSection 컴포넌트화

---
+++ components/CardSection.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+function CardSection({
+  title,
+  children,
+}: {
+  title?: ReactNode;
+  children: ReactNode;
+}) {
+  return (
+    <div className="bg-white rounded-2xl p-4 space-y-6">
+      {title}
+      {children}
+    </div>
+  );
+}
+
+export default CardSection;
