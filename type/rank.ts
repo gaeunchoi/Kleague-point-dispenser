5f4b1441ca6920a1c731b91e57f0511dc64f62e4
gaeunchoi <gaeun1884@naver.com>
2025-05-23 22:53:55 +0900

    Feat: mockData 생성

---
+++ type/rank.ts
@@ -0,0 +1,12 @@
+export interface Rank {
+  rank: number;
+  teamName: string;
+  totalGame: number;
+  winCnt: number;
+  drawCnt: number;
+  loseCnt: number;
+  score: number;
+  conceded: number;
+  goalDiff: number;
+  points: number;
+}
