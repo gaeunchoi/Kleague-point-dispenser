d3079e8a75c143e0967a789605263cb4ac83e514
gaeunchoi <gaeun1884@naver.com>
2025-05-23 17:30:25 +0900

    Feat: 제목-결과값 매칭되는 StatsInfo 컴포넌트화

---
+++ components/StatsInfo.tsx
@@ -0,0 +1,15 @@
+type StatsInfoProps = {
+  title: string;
+  displayValue: string;
+};
+
+function StatsInfo({ title, displayValue }: StatsInfoProps) {
+  return (
+    <div className="flex flex-row place-content-between mb-3">
+      <span className="text-md text-gray-600">{title}</span>
+      <span className="text-xl font-bold">{displayValue}</span>
+    </div>
+  );
+}
+
+export default StatsInfo;
