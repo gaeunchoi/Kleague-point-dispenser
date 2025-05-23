454cab5f94d500243312170838c6d901b62167a0
gaeunchoi <gaeun1884@naver.com>
2025-05-23 17:31:22 +0900

    Feat: ProgressBar 숫자 단위 구분 기능 추가

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,20 +1,32 @@
+import StatsInfo from "./StatsInfo";
+
 export type ProgressBarProps = {
   color: string;
+  title: string;
   value: number;
   max: number;
+  showUnit?: "percent" | "score";
 };
 
-function ProgressBar({ color, value, max }: ProgressBarProps) {
-  const percentage = (value / max) * 100;
-  console.log(percentage);
+function ProgressBar({ color, title, value, max, showUnit }: ProgressBarProps) {
+  const percentage = ((value / max) * 100).toFixed(1);
+
+  const displayValueWithUnit = () => {
+    if (showUnit === "percent") return `${percentage}%`;
+    if (showUnit === "score") return `${value}점`;
+    return `${value}`;
+  };
 
   return (
-    <div className="h-2 w-full rounded-full bg-gray-300 overflow-hidden">
-      <div
-        className="h-full"
-        style={{ backgroundColor: `${color}`, width: `${percentage}%` }}
-      ></div>
-    </div>
+    <>
+      <StatsInfo title={title} displayValue={displayValueWithUnit()} />
+      <div className="h-2 w-full rounded-full bg-gray-300 overflow-hidden">
+        <div
+          className={`h-full ${color}`}
+          style={{ width: `${percentage}%` }}
+        ></div>
+      </div>
+    </>
   );
 }
 
