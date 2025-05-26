465b109de5835bea0253bb1391a7c2d5b4f94db4
gaeunchoi <gaeun1884@naver.com>
2025-05-26 21:30:37 +0900

    Feat: 팀 로고 이미지파일 추가

---
+++ data/teamLogo.ts
@@ -0,0 +1,18 @@
+const TeamLogos: { [key: string]: string } = {
+  경남: "/img/gyeongnam.png",
+  김포: "/img/gimpo.png",
+  부산: "/img/busan.png",
+  부천: "/img/bucheon.png",
+  서울E: "/img/seoul-eland.png",
+  성남: "/img/seongnam.png",
+  수원: "/img/suwon.png",
+  안산: "/img/ansan.png",
+  인천: "/img/incheon.png",
+  전남: "/img/jeonnam.png",
+  충남아산: "/img/chungnam-asan.png",
+  충북청주: "/img/chungbuk-cheongju.png",
+  천안: "/img/cheonan.png",
+  화성: "/img/hwaseong.png",
+};
+
+export default TeamLogos;
