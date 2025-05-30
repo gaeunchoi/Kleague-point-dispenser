a479831d11b40c451fc7690a211ed81e82c980ef
gaeunchoi <gaeun1884@naver.com>
2025-05-30 19:53:30 +0900

    Feat: K1, 2리그 API 반영 + zustand 상태 수정

--- app/api/fetchLeagueData/route.ts
+++ app/api/fetchLeagueData/route.ts
@@ -4,25 +4,36 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    const res = await axios.get(
+    const k1res = await axios.get(
+      "https://www.kleague.com/record/teamRank.do?leagueId=1&year=2025&stadium=all&recordType=rank"
+    );
+
+    const k2res = await axios.get(
       "https://www.kleague.com/record/teamRank.do?leagueId=2&year=2025&stadium=all&recordType=rank"
     );
-    const teamRank = res.data?.data?.teamRank;
 
-    const leagueRankInfo: Rank[] = teamRank.map((team: any) => ({
-      leagueId: team.leagueId,
-      rank: team.rank,
-      teamName: team.teamName,
-      gameCnt: team.gameCount,
-      winCnt: team.winCnt,
-      tieCnt: team.tieCnt,
-      lossCnt: team.lossCnt,
-      gainGoal: team.gainGoal,
-      lossGoal: team.lossGoal,
-      gapGoal: team.gapCnt,
-      gainPoint: team.gainPoint,
-    }));
-    return NextResponse.json(leagueRankInfo);
+    const k1TeamRank = k1res.data?.data?.teamRank;
+    const k2TeamRank = k2res.data?.data?.teamRank;
+
+    const getLeagueTeamRank = (teamRank: any[]): Rank[] =>
+      teamRank.map((team: any) => ({
+        leagueId: team.leagueId,
+        rank: team.rank,
+        teamName: team.teamName,
+        gameCnt: team.gameCount,
+        winCnt: team.winCnt,
+        tieCnt: team.tieCnt,
+        lossCnt: team.lossCnt,
+        gainGoal: team.gainGoal,
+        lossGoal: team.lossGoal,
+        gapGoal: team.gapCnt,
+        gainPoint: team.gainPoint,
+      }));
+
+    const k1leagueRankInfo = getLeagueTeamRank(k1TeamRank || []);
+    const k2leagueRankInfo = getLeagueTeamRank(k2TeamRank || []);
+
+    return NextResponse.json({ k1: k1leagueRankInfo, k2: k2leagueRankInfo });
   } catch (e) {
     console.error("🚨 에러 발생: ", e);
     return NextResponse.json({ error: "데이터를 불러오지 못했습니다." });
--- store/leagueStore.ts
+++ store/leagueStore.ts
@@ -1,19 +1,45 @@
+import { LeagueId } from "@/data/teamLogo";
 import { Rank } from "@/type/rank";
 import { create } from "zustand";
 
 type LeagueStore = {
-  data: Rank[];
+  k1Data: Rank[];
+  k2Data: Rank[];
+  curLeagueData: Rank[];
   isLoading: boolean;
   fetchData: () => void;
+
+  leagueId: LeagueId;
+  toggleLeagueId: () => void;
 };
 
-export const useLeagueStore = create<LeagueStore>((set) => ({
-  data: [],
+export const useLeagueStore = create<LeagueStore>((set, get) => ({
+  k1Data: [],
+  k2Data: [],
+  curLeagueData: [],
   isLoading: true,
   fetchData: async () => {
     set({ isLoading: true });
-    const res = await fetch("/api/fetchLeagueData");
-    const data = await res.json();
-    set({ data, isLoading: false });
+    try {
+      const res = await fetch("/api/fetchLeagueData");
+      const { k1, k2 } = await res.json();
+      set({
+        k1Data: k1,
+        k2Data: k2,
+        isLoading: false,
+        leagueId: "K1",
+        curLeagueData: k1,
+      });
+    } catch (err) {
+      console.error("🚨 fetchData 에러: ", err);
+      set({ isLoading: false });
+    }
+  },
+  leagueId: "K1",
+  toggleLeagueId: () => {
+    const state = get();
+    const newId: LeagueId = state.leagueId === "K1" ? "K2" : "K1";
+    const newData = newId === "K1" ? state.k1Data : state.k2Data;
+    set({ leagueId: newId, curLeagueData: newData });
   },
 }));
