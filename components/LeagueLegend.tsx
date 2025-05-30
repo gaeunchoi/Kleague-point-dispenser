adce39348283aa466ef6fe9f37e3be5150688db4
gaeunchoi <gaeun1884@naver.com>
2025-05-30 16:44:22 +0900

    Design: 리그 순위 legend flex-direction 변경

--- components/LeagueLegend.tsx
+++ components/LeagueLegend.tsx
@@ -1,9 +1,9 @@
 import { cn } from "@/utils/cn";
-import { smLabel, flexCol, flexRowCenter } from "./styles";
+import { smLabel, flexRowCenter, flexRow } from "./styles";
 
 function LeagueLegend() {
   return (
-    <div className={flexCol("my-4", "ml-6")}>
+    <div className={flexRow("gap-8", "my-4", "ml-6")}>
       <div className={flexRowCenter()}>
         <div className={cn("min-w-2", "min-h-2", "bg-blue-500", "rounded")} />
         <span className={smLabel()}>다이렉트 승격</span>
