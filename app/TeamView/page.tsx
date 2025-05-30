734fa0c263ba5ff7df7ab15b3065d168969ddfff
gaeunchoi <gaeun1884@naver.com>
2025-05-30 20:14:52 +0900

    Feat: 메인 페이지 생성 + 기존 팀 정보 페이지 생성

---
+++ app/TeamView/page.tsx
@@ -0,0 +1,9 @@
+import PointDispenser from "@/components/PointDispenser";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen min-w-[340px] bg-[#f9fafb]">
+      <PointDispenser />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
-import PointDispenser from "@/components/PointDispenser";
+"use client";
+import { flexColCenter, flexRow } from "@/components/styles";
+import { useLeagueStore } from "@/store/leagueStore";
+import { cn } from "@/utils/cn";
+import { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
+import LeagueRankTable from "@/components/LeagueRankTable";
+import TeamSwiper from "@/components/TeamSwiper";
 import Head from "next/head";
 
 export default function Home() {
+  const { isLoading, fetchData, leagueId, toggleLeagueId } = useLeagueStore();
+  const [showTable, setShowTable] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <ClipLoader color="#0066b3" size={48} />
+      </div>
+    );
+  }
+
   return (
     <>
       <Head>
@@ -13,9 +35,34 @@ export default function Home() {
           content="K리그 팀의 우승확률 및 경기 정보를 확인하세요"
         />
       </Head>
-      <main className="min-h-screen min-w-[340px] bg-[#f9fafb]">
-        <PointDispenser />
-      </main>
+      <div
+        className={flexColCenter("justify-center", "w-full", "gap-3", "p-4")}
+      >
+        <div className={flexRow("justify-center", "mt-4")}>
+          <button
+            onClick={() => toggleLeagueId()}
+            className={cn(leagueId === "K1" && "font-bold")}
+          >
+            K리그 1
+          </button>
+          <button
+            onClick={() => toggleLeagueId()}
+            className={cn({
+              "font-bold ml-4": leagueId === "K2",
+              "ml-4": leagueId !== "K2",
+            })}
+          >
+            K리그 2
+          </button>
+        </div>
+        {showTable ? <LeagueRankTable /> : <TeamSwiper />}
+        <button
+          onClick={() => setShowTable((prev) => !prev)}
+          className={cn("bg-blue-200", "p-4", "rounded-2xl")}
+        >
+          {showTable ? "슬라이드로 보기" : "순위표로 보기"}
+        </button>
+      </div>
     </>
   );
 }
