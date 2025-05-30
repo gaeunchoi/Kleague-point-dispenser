a3ed68d3b168eed7af62a830e04a40bd382b88fb
gaeunchoi <gaeun1884@naver.com>
2025-05-30 20:37:00 +0900

    Fix: TeamView Suspense 추가

--- app/TeamView/page.tsx
+++ app/TeamView/page.tsx
@@ -1,9 +1,12 @@
 import PointDispenser from "@/components/PointDispenser";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="min-h-screen min-w-[340px] bg-[#f9fafb]">
-      <PointDispenser />
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <PointDispenser />
+      </Suspense>
     </div>
   );
 }
