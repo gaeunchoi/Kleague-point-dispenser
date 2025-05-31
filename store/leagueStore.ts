e73ab77cee0c8a5ec95464c3eea2639b7b075185
gaeunchoi <gaeun1884@naver.com>
2025-05-31 14:11:10 +0900

    Feat: zustand 미들웨어 사용 -> persist 추가

--- store/leagueStore.ts
+++ store/leagueStore.ts
@@ -1,6 +1,7 @@
 import { LeagueId } from "@/data/teamLogo";
 import { Rank } from "@/type/rank";
 import { create } from "zustand";
+import { createJSONStorage, persist, PersistOptions } from "zustand/middleware";
 
 type LeagueStore = {
   k1Data: Rank[];
@@ -13,33 +14,42 @@ type LeagueStore = {
   toggleLeagueId: () => void;
 };
 
-export const useLeagueStore = create<LeagueStore>((set, get) => ({
-  k1Data: [],
-  k2Data: [],
-  curLeagueData: [],
-  isLoading: true,
-  fetchData: async () => {
-    set({ isLoading: true });
-    try {
-      const res = await fetch("/api/fetchLeagueData");
-      const { k1, k2 } = await res.json();
-      set({
-        k1Data: k1,
-        k2Data: k2,
-        isLoading: false,
-        leagueId: "K1",
-        curLeagueData: k1,
-      });
-    } catch (err) {
-      console.error("🚨 fetchData 에러: ", err);
-      set({ isLoading: false });
+export const useLeagueStore = create<LeagueStore>()(
+  persist(
+    (set, get) => ({
+      k1Data: [],
+      k2Data: [],
+      curLeagueData: [],
+      isLoading: true,
+      fetchData: async () => {
+        set({ isLoading: true });
+        try {
+          const res = await fetch("/api/fetchLeagueData");
+          const { k1, k2 } = await res.json();
+          const curLeague = get().leagueId === "K1" ? k1 : k2;
+
+          set({
+            k1Data: k1,
+            k2Data: k2,
+            curLeagueData: curLeague,
+            isLoading: false,
+          });
+        } catch (err) {
+          console.error("🚨 fetchData 에러: ", err);
+          set({ isLoading: false });
+        }
+      },
+      leagueId: "K1",
+      toggleLeagueId: () => {
+        const state = get();
+        const newId: LeagueId = state.leagueId === "K1" ? "K2" : "K1";
+        const newData = newId === "K1" ? state.k1Data : state.k2Data;
+        set({ leagueId: newId, curLeagueData: newData });
+      },
+    }),
+    {
+      name: "league-store",
+      storage: createJSONStorage(() => sessionStorage),
     }
-  },
-  leagueId: "K1",
-  toggleLeagueId: () => {
-    const state = get();
-    const newId: LeagueId = state.leagueId === "K1" ? "K2" : "K1";
-    const newData = newId === "K1" ? state.k1Data : state.k2Data;
-    set({ leagueId: newId, curLeagueData: newData });
-  },
-}));
+  )
+);
