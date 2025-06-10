7fbf37d156f2e6ea437ef96cf0c5a0e128d68a97
gaeunchoi <gaeun1884@naver.com>
2025-06-10 19:58:36 +0900

    Fix: og 진짜로 적용

--- app/page.tsx
+++ app/page.tsx
@@ -21,35 +21,22 @@ export default function Home() {
   if (isLoading) return <LoadingSpinner h="h-screen" />;
 
   return (
-    <>
-      <Head>
-        <title>K리그 승점자판기</title>
-        <meta property="og:image" content="/thumbnail.png" />
-        <meta property="og:title" content="K리그 승점자판기" />
-        <meta
-          property="og:description"
-          content="K리그 팀의 우승확률 및 경기 정보를 확인하세요"
+    <div className={flexColCenter("justify-center", "w-full", "gap-3", "p-6")}>
+      <LeagueToggle />
+      {showRankTable ? (
+        <TabContent
+          tabs={["리그 순위표", "리그 일정표"]}
+          contents={[<LeagueRankTable key="0" />, <LeagueSchedule key="1" />]}
         />
-      </Head>
-      <div
-        className={flexColCenter("justify-center", "w-full", "gap-3", "p-6")}
+      ) : (
+        <TeamSwiper />
+      )}
+      <button
+        onClick={() => setShowRankTable((prev) => !prev)}
+        className={mainPageBtn("px-6", "py-2", "fixed", "bottom-7")}
       >
-        <LeagueToggle />
-        {showRankTable ? (
-          <TabContent
-            tabs={["리그 순위표", "리그 일정표"]}
-            contents={[<LeagueRankTable key="0" />, <LeagueSchedule key="1" />]}
-          />
-        ) : (
-          <TeamSwiper />
-        )}
-        <button
-          onClick={() => setShowRankTable((prev) => !prev)}
-          className={mainPageBtn("px-6", "py-2")}
-        >
-          {showRankTable ? "슬라이드로 보기" : "순위표로 보기"}
-        </button>
-      </div>
-    </>
+        {showRankTable ? "슬라이드로 보기" : "순위표로 보기"}
+      </button>
+    </div>
   );
 }
